a96008477f133f134fab8c63c7eaa8c752587e6e
Yuichi Murata <mrk21info+github@gmail.com>
2021-06-27 01:15:16 +0900

    Fix peco config

--- zsh/zshrc.d/peco.sh
+++ zsh/zshrc.d/peco.sh
@@ -11,7 +11,7 @@ if which peco > /dev/null; then
 
   # directory search
   function peco-cdr() {
-    local selected_dir="$(cdr -l | sed 's/^[0-9]\+ \+//' | peco --prompt="cdr >" --query "$LBUFFER")"
+    local selected_dir="$(cdr -l | sed -E 's/^[0-9]+ +//' | peco --prompt="cdr >" --query "$LBUFFER")"
     if [ -n "$selected_dir" ]; then
       BUFFER="cd ${selected_dir}"
       zle accept-line
